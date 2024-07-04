af65db1f18e7a658775d79309a99ffb6c7f67708
Nuno Machado <nvmcv@hotmail.com>
2024-07-04 02:58:36 +0100

    minor changes

--- evo_functions.py
+++ evo_functions.py
@@ -109,7 +109,7 @@ def print_memory_stats():
 
 # function to convert string to be tokenized from the format expected by the mistral model to the 
 # format expected by the phi 3 model
-def convert_text_mistral_phi3(input_string, tok):
+def convert_text_mistral_phi3(input_string):
     # Check if both start and end markers are present
     start_index = input_string.find('[INST]')
     end_index = input_string.find('[/INST]')
@@ -128,7 +128,10 @@ def convert_text_mistral_phi3(input_string, tok):
     system_part_index = content.find('\n')
     
     # Create the message dictionary
-    phi_format = f"<|system|>\n{content[:system_part_index]}<|end|>\n<|user|>\n{content[system_part_index+2:]}<|end|>\n<|assistant|>{input_string[end_index+7:]}"
+    phi_format = f"<s> <|system|>\n{content[:system_part_index]}<|end|>\n<|user|>\n{content[system_part_index+2:]}<|end|>\n<|assistant|>{input_string[end_index+7:]}"
+    #phi_format = f"<|system|>\n{content[:system_part_index]}<|end|>\n<|user|>\n{content[system_part_index+2:]}<|end|>\n<|assistant|>{input_string[end_index+7:]}"
+    #phi_format = f"<s> <|user|>\n{content}<|end|>\n<|assistant|>{input_string[end_index+7:]}"
+    #phi_format = f"<|user|>\n{content}<|end|>\n<|assistant|>{input_string[end_index+7:]}"
     message = [{"role": "user", "content": content},
                 {"role": "assistant", "content":  input_string[end_index+7:]},]
     
@@ -1443,7 +1446,7 @@ def new_mutate_prompt(prompt,
                       mutation_prompt_dict, # just  a dict with the repective parts names
                       model, 
                       tokenizer):
-    print(f"NEW MUTATE BOS")
+    #print(f"NEW MUTATE BOS")
 
     # case with empty string in self reflection prompts
     if prompt == '' or prompt == ' ':
@@ -1473,7 +1476,7 @@ def new_mutate_prompt(prompt,
 
     new_tokens = output[0, prompt_length:]
     mutated = tokenizer.decode(new_tokens, skip_special_tokens=True)
-    print(f"NEW MUTATE PROMPT)-->{tokenizer.decode(output[0], skip_special_tokens=False)}")
+    #print(f"NEW MUTATE PROMPT)-->{tokenizer.decode(output[0], skip_special_tokens=False)}")
 
     mutated = mutated.lstrip()
 
--- tester.py
+++ tester.py
@@ -1,35 +1,20 @@
-import torch 
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline 
+import os
+# set available gpu's
+os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 
-torch.random.manual_seed(0) 
-model = AutoModelForCausalLM.from_pretrained( 
-    "microsoft/Phi-3-mini-128k-instruct",  
-    device_map="cuda",  
-    torch_dtype="auto",  
-    trust_remote_code=True,  
-) 
+from evo_functions import load_model
 
-tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct") 
+model, tokenizer = load_model(checkpoint = "microsoft/Phi-3-mini-4k-instruct", quantized = True)
 
-messages = [ 
-    {"role": "system", "content": "You are a helpful AI assistant."}, 
-    {"role": "user", "content": "Can you provide ways to eat combinations of bananas and dragonfruits?"}, 
-    {"role": "assistant", "content": "Sure! Here are some ways to eat bananas and dragonfruits together: 1. Banana and dragonfruit smoothie: Blend bananas and dragonfruits together with some milk and honey. 2. Banana and dragonfruit salad: Mix sliced bananas and dragonfruits together with some lemon juice and honey."}, 
-    {"role": "user", "content": "What about solving an 2x + 3 = 7 equation?"}, 
-] 
 
-pipe = pipeline( 
-    "text-generation", 
-    model=model, 
-    tokenizer=tokenizer, 
-) 
+bos = [tokenizer.bos_token_id]
+eos = [tokenizer.eos_token_id]
 
-generation_args = { 
-    "max_new_tokens": 500, 
-    "return_full_text": False, 
-    "temperature": 0.0, 
-    "do_sample": False, 
-}
+print(f"BOS-->{bos}")
+print(f"EOS-->{eos}")
 
-output = pipe(messages, **generation_args) 
-print(output[0])
+print("after decoding")
+bos_conv = tokenizer.decode(bos, skip_special_tokens=False)
+eos_conv = tokenizer.decode(eos, skip_special_tokens=False)
+print(f"BOS after-->{bos_conv}")
+print(f"EOS after-->{eos_conv}")

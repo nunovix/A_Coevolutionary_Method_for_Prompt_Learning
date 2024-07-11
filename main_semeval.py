ce7ba4f804b0051b9262f6d900e5e0a62e054efc
Nuno Machado <nvmcv@hotmail.com>
2024-07-11 13:48:43 +0100

    changes to run baseline and algorithm variatons with 0's and/or 1 for the mut and cross probs

--- main_semeval.py
+++ main_semeval.py
@@ -1,6 +1,6 @@
 import os
 # set available gpu's
-os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 import evo_functions as evo
@@ -35,7 +35,7 @@ best_prompt, best_score_iterations = evo.evo_alg_2(task = "SemEval",
                                                             n_top = 5,
                                                             mutation_prob=0.5,
                                                             crossover_prob=0.0,
-                                                            sampling_T = None,
+                                                            sampling_T = 5.0,
                                                             patience = 10,
                                                             max_iter = 200,
                                                             data_size = 0, # number of examples where the prompts are evaluate 0 means all
@@ -56,7 +56,7 @@ best_prompt, best_score_iterations = evo.evo_alg_2(task = "SemEval",
                                                             n_top = 5,
                                                             mutation_prob=0.0,
                                                             crossover_prob=0.5,
-                                                            sampling_T = None,
+                                                            sampling_T = 5.0,
                                                             patience = 10,
                                                             max_iter = 200,
                                                             data_size = 0, # number of examples where the prompts are evaluate 0 means all
@@ -69,15 +69,15 @@ best_prompt, best_score_iterations = evo.evo_alg_2(task = "SemEval",
                                                             save=True,
                                                             )
 
-# sempre mutação
+
 best_prompt, best_score_iterations = evo.evo_alg_2(task = "SemEval", 
                                                             model_name = "microsoft/Phi-3-mini-4k-instruct",
                                                             quantize_model_4bits = True,
                                                             n_pop = 25,
                                                             n_top = 5,
-                                                            mutation_prob=0.0,
-                                                            crossover_prob=0.5,
-                                                            sampling_T = None,
+                                                            mutation_prob=0.5,
+                                                            crossover_prob=1.0,
+                                                            sampling_T = 5.0,
                                                             patience = 10,
                                                             max_iter = 200,
                                                             data_size = 0, # number of examples where the prompts are evaluate 0 means all
@@ -90,15 +90,15 @@ best_prompt, best_score_iterations = evo.evo_alg_2(task = "SemEval",
                                                             save=True,
                                                             )
 
-# sempre crossover
+# só crossover
 best_prompt, best_score_iterations = evo.evo_alg_2(task = "SemEval", 
                                                             model_name = "microsoft/Phi-3-mini-4k-instruct",
                                                             quantize_model_4bits = True,
                                                             n_pop = 25,
                                                             n_top = 5,
-                                                            mutation_prob=0.0,
+                                                            mutation_prob=1.0,
                                                             crossover_prob=0.5,
-                                                            sampling_T = None,
+                                                            sampling_T = 5.0,
                                                             patience = 10,
                                                             max_iter = 200,
                                                             data_size = 0, # number of examples where the prompts are evaluate 0 means all
